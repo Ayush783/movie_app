9cf4fb9853a5e2a9e95e3cd0c653b9cbc1790026
Ayush783 <ayushc45xxx@gmail.com>
2020-08-12 15:09:18 +0530

    created movies model

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_app/widgets/movie_card.dart';
 
 import '../const.dart';
 
@@ -12,7 +11,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
