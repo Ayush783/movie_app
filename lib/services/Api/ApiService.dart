2c9efa62b7111b100fb18b7b12157896bf4880cd
Ayush783 <ayushc45xxx@gmail.com>
2020-08-12 15:24:52 +0530

    completed api service to get some movies

---
+++ lib/services/Api/ApiService.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:movie_app/models/movie_list.dart';
+import 'package:movie_app/services/Api/api_key.dart';
+
+class ApiService {
+  String baseUrl = 'https://api.themoviedb.org/3';
+
+  Future<MovieResponse> getNowPlayingMovies() async {
+    try {
+      http.Response response =
+          await http.get('$baseUrl/movie/now_playing?api_key=$apiKey&page=1');
+      return MovieResponse.fromJson(jsonDecode(response.body));
+    } catch (error) {
+      return MovieResponse.withError("$error");
+    }
+  }
+
+  Future<MovieResponse> getMovies() async {
+    try {
+      http.Response response =
+          await http.get('$baseUrl/movie/top_rated?api_key=$apiKey&page=1');
+      return MovieResponse.fromJson(jsonDecode(response.body));
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return MovieResponse.withError("$error");
+    }
+  }
+}
