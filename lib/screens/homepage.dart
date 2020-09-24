a5a0110f7c1a389fbfb42b1fbaa7f73eac286b45
Ayush783 <ayushc45xxx@gmail.com>
2020-09-25 03:32:33 +0530

    top rated actors widget complete

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app/bloc/person_bloc/person_bloc.dart';
 import 'package:movie_app/const.dart';
-import 'package:movie_app/models/person.dart';
+import 'package:movie_app/widgets/heading.dart';
+import 'package:movie_app/widgets/top_rated_actors.dart';
 import 'package:movie_app/widgets/trending_movies.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,39 +10,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final bloc = PersonBloc();
-
-  @override
-  void initState() {
-    super.initState();
-    bloc.add(GetPopularPersonMale());
-  }
-
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
         TrendingMovies(),
         padding,
-        BlocBuilder<PersonBloc, PersonState>(
-          cubit: bloc,
-          builder: (context, state) {
-            if (state is PersonLoaded) {
-              return Container(
-                child: Wrap(
-                  children: [
-                    for (Person person in state.response.list)
-                      Text(
-                        person.name,
-                        style: primary,
-                      ),
-                  ],
-                ),
-              );
-            }
-            return Text('');
-          },
-        )
+        Heading(
+          heading: 'Top-Rated Male Actors',
+        ),
+        BuildTopRatedMaleActorWidget()
       ],
     );
   }
