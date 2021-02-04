e2e466c7f2fbf8e36863fde5325eac2c3fa611e7
Ayush783 <ayushc45xxx@gmail.com>
2021-02-04 21:02:04 +0530

    updated bloc list

--- lib/screens/dashboard/bloc_list.dart
+++ lib/screens/dashboard/bloc_list.dart
@@ -3,6 +3,10 @@ import 'package:movie_app/bloc/movie/now_playing_movies_bloc/nowplayingmovies_bl
 import 'package:movie_app/bloc/movie/popular_movies_bloc/popularmovies_bloc.dart';
 import 'package:movie_app/bloc/movie/top_rated_movies_bloc/topratedmovies_bloc.dart';
 import 'package:movie_app/bloc/movie/upcoming_movies_bloc/upcomingmovies_bloc.dart';
+import 'package:movie_app/bloc/tv/airing_now_bloc/airingnowtv_bloc.dart';
+import 'package:movie_app/bloc/tv/airing_today_bloc/airtodaytv_bloc.dart';
+import 'package:movie_app/bloc/tv/popular_tv_bloc/populartv_bloc.dart';
+import 'package:movie_app/bloc/tv/top_rated_tv_bloc/topratedtv_bloc.dart';
 import 'package:movie_app/injection.dart';
 
 List<BlocProvider> blocProviders = [
@@ -25,5 +29,25 @@ List<BlocProvider> blocProviders = [
       create: (context) => getIt<UpcomingmoviesBloc>()
         ..add(
           UpcomingmoviesEvent.getUpcomingMovies(),
+        )),
+  BlocProvider<AiringnowtvBloc>(
+      create: (context) => getIt<AiringnowtvBloc>()
+        ..add(
+          AiringnowtvEvent.getTVshows(),
+        )),
+  BlocProvider<AirtodaytvBloc>(
+      create: (context) => getIt<AirtodaytvBloc>()
+        ..add(
+          AirtodaytvEvent.getTVshows(),
+        )),
+  BlocProvider<PopulartvBloc>(
+      create: (context) => getIt<PopulartvBloc>()
+        ..add(
+          PopulartvEvent.getTVshows(),
+        )),
+  BlocProvider<TopratedtvBloc>(
+      create: (context) => getIt<TopratedtvBloc>()
+        ..add(
+          TopratedtvEvent.getTVshows(),
         )),
 ];
