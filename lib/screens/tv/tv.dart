919dacee9d4c5264132276821b93db16f6b4817f
Ayush783 <ayushc45xxx@gmail.com>
2021-02-04 21:12:38 +0530

    carousel built for tv shows

--- lib/screens/tv/tv.dart
+++ lib/screens/tv/tv.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/screens/tv/widgets/carousel.dart';
 
 class TVScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          //custom carousel widget
+          Carousel(),
+          //upcoming movies section
+          // AiringNowTVShows(),
+          //most popular movies section
+          // PopularTVShows(),
+          //top rated movies section
+          // TopRatedTVShows(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/screens/tv/widgets/carousel.dart
@@ -0,0 +1,152 @@
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/bloc/tv/airing_today_bloc/airtodaytv_bloc.dart';
+import 'package:movie_app/entities/tv/tv.dart';
+
+class Carousel extends StatelessWidget {
+  final ValueNotifier<int> _index = ValueNotifier(0);
+  final PageController _controller = PageController(initialPage: 1000);
+
+  @override
+  Widget build(BuildContext context) {
+    // _controller.addListener(() async {
+    //   print(_controller.page);
+    //   await Future.delayed(Duration(seconds: 3));
+    //   _controller.nextPage(
+    //     duration: Duration(milliseconds: 300),
+    //     curve: Curves.easeInOut,
+    //   );
+    // });
+    final size = MediaQuery.of(context).size;
+    final bloc = BlocProvider.of<AirtodaytvBloc>(context);
+    return BlocBuilder<AirtodaytvBloc, AirtodaytvState>(
+        builder: (context, state) {
+      return state.map(
+          initial: (_) => Container(),
+          loading: (_) => Container(
+              height: size.height / 3,
+              child: Center(
+                  child: CircularProgressIndicator(
+                backgroundColor: Color(0xffff0000),
+              ))),
+          loaded: (state) {
+            final List<TV> shows = state.shows;
+            return _buildBody(size, shows);
+          },
+          error: (state) {
+            return Center(child: Text('Error'));
+          });
+    });
+  }
+
+  String posterUrl(String val) {
+    return 'https://image.tmdb.org/t/p/w500/${val}';
+  }
+
+  String backPosterUrl(String val) {
+    return 'https://image.tmdb.org/t/p/w500/${val}';
+  }
+
+  Container _buildBody(Size size, List<TV> shows) {
+    return Container(
+      height: size.height / 3,
+      width: size.width,
+      child: Stack(
+        children: [
+          PageView.builder(
+            controller: _controller,
+            itemBuilder: (context, index) => Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: CachedNetworkImageProvider(
+                    backPosterUrl(shows[index % 10].backdrop_path),
+                  ),
+                  fit: BoxFit.fill,
+                  colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.8), BlendMode.dst),
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 3,
+                  sigmaY: 3,
+                ),
+                child: Container(
+                  color: Colors.white.withOpacity(0.0),
+                  padding: EdgeInsets.only(
+                    left: size.width / 20,
+                    top: size.height / 50,
+                    bottom: size.height / 14,
+                    right: size.width / 1.55,
+                  ),
+                  child: PhysicalModel(
+                    elevation: 20,
+                    color: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: CachedNetworkImageProvider(
+                            posterUrl(shows[index % 10].poster_path),
+                          ),
+                          fit: BoxFit.fill,
+                          colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.8), BlendMode.dst),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            onPageChanged: (i) {
+              _index.value = i % 10;
+            },
+          ),
+          Positioned(
+            bottom: 20,
+            child: ValueListenableBuilder<int>(
+              valueListenable: _index,
+              builder: (context, value, child) => Container(
+                  height: 30,
+                  width: size.width,
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: PhysicalModel(
+                    elevation: 20,
+                    shadowColor: Colors.transparent,
+                    color: Colors.transparent,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        for (int i = 0; i < 10; i++)
+                          GestureDetector(
+                            onTap: () {
+                              _controller.animateToPage(i,
+                                  duration: Duration(milliseconds: 300),
+                                  curve: Curves.easeInOutSine);
+                            },
+                            child: Container(
+                              height: 18,
+                              width: 18,
+                              decoration: BoxDecoration(
+                                color: _index.value == i
+                                    ? Colors.red
+                                    : Colors.black,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
+                  )),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
